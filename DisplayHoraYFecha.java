d650498ff849bb6dbc3fc8a293ca69199e10bfb1
Oscar Charro Rivera <oschariv@gmail.com>
2016-11-17 13:09:33 +0100

    0570.a.distintas formas de mostrar el display con el metodo setMomento()

--- DisplayHoraYFecha.java
+++ DisplayHoraYFecha.java
@@ -11,20 +11,47 @@ public class DisplayHoraYFecha
     private DisplayDosDigitos dia;
     private DisplayDosDigitos mes;
     private DisplayDosDigitos anno;
+    
+    private boolean mostrarHora;
+    private boolean mostrarFecha;
    
     
     
     /**
-     * Constructor for objects of class DisplayHoraYFecha
+     * Constructor for objects of class DisplayHoraYFecha.
+     * Seleccionar true o false para los parametros.
+     * true - true -> muestra fecha y hora.
+     * true - false -> solo muestra la hora.
+     * false - true -> solo muestra la fecha.
+     * false - false -> no muestra nada.
      */
-    public DisplayHoraYFecha()
+    public DisplayHoraYFecha(boolean soloHora, boolean soloFecha)
     {
         horas = new NumberDisplay(24);
         minutos = new NumberDisplay(60);
         dia = new DisplayDosDigitos(31);
         mes = new DisplayDosDigitos(13);
         anno = new DisplayDosDigitos(100);
+        
+        mostrarHora = soloHora;
+        mostrarFecha = soloFecha;
+        
+        if (mostrarHora == true && mostrarFecha == true) {
+            System.out.println("Se mostrara fecha y hora.");
+        }
+        else if (mostrarHora == true && mostrarFecha == false) {
+            System.out.println("Se mostrara solo la hora.");
+        }
+        else if (mostrarHora == false && mostrarFecha == true) {
+            System.out.println("Se mostrara sola la fecha.");
+        }
+        else {
+            System.out.println("No se mostra nada.");
+        }
+        
     }
+    
+    
 
     /**
      * Metodo avanzarMomento
@@ -72,8 +99,24 @@ public class DisplayHoraYFecha
         
      public String getMomento()
     {
-        return horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " +
-		       dia.getValorDelDisplay() + "/" + mes.getValorDelDisplay() + "/" +
-		       anno.getValorDelDisplay();
+        String momento;
+        
+        if (mostrarHora == true && mostrarFecha == true) {
+            momento = horas.getDisplayValue() + ":" + minutos.getDisplayValue() 
+             + " " + dia.getValorDelDisplay() + "/" + mes.getValorDelDisplay() 
+             + "/" + anno.getValorDelDisplay();
+        }
+        else if (mostrarHora == true && mostrarFecha == false) {
+            return horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        }
+        else if (mostrarHora == false && mostrarFecha == true) {
+            momento = dia.getValorDelDisplay() + "/" + mes.getValorDelDisplay() 
+                + "/" + anno.getValorDelDisplay();
+        }
+        else{
+            momento = "";
+        }
+        
+        return momento; 
     }
 }
